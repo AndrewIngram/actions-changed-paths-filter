f79fab426427beb291704bb22cf25d8a3d6fcae8
Andrew Ingram <andy@andrewingram.net>
2019-03-16 18:07:21 +0000

    support multiple patterns

--- Dockerfile
+++ Dockerfile
@@ -1,6 +1,10 @@
 FROM debian:stable-slim
 
-LABEL "com.github.actions.name"="Change Filter for GitHub Actions"
+LABEL "repository"="https://github.com/AndrewIngram/actions-changed-directory-filter"
+LABEL "homepage"="https://github.com/AndrewIngram/actions-changed-directory-filter"
+LABEL "maintainer"="Andrew Ingram <andy@andrewingram.net>"
+
+LABEL "com.github.actions.name"="Changed Directory Filter for GitHub Actions"
 LABEL "com.github.actions.description"="Filter based on changes against the base branch"
 LABEL "com.github.actions.icon"="filter"
 LABEL "com.github.actions.color"="gray-dark"
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,14 +1,23 @@
 #!/bin/sh -l
 
-if [ -n $BASE_BRANCH ]; then
-  echo "BASE_BRANCH environment variable not set, using \"master\""
+if [ -z ${BASE_BRANCH+x} ]; then
+  echo "- BASE_BRANCH environment variable not set, using \"master\""
   BASE_BRANCH=master
 fi
 
-if git diff --name-only origin/$BASE_BRANCH...$GITHUB_SHA | grep -F $1; then
-    echo "At least 1 changed file matched the provided pattern"
+ALL_CHANGES=()
+
+for expression in "$@"; do
+    ALL_CHANGES+=($(git diff --name-only origin/$BASE_BRANCH...$GITHUB_SHA | grep ^$expression))
+done
+
+if (( ${#ALL_CHANGES[@]} )); then
+    echo "- At least 1 changed file matched the provided pattern(s):"
+    for i in "${ALL_CHANGES[@]}"; do
+        echo "  - $i"
+    done
     exit 0
 else
-    echo "No changed files match the provided pattern"
+    echo "- No changed files match the provided pattern"
     exit 78
 fi
